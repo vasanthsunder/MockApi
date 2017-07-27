dd2758d2ebbb03a13445fa2e18a19631a4c2eb80
vasanthsunder <vasanth.sunder@verizon.com>
2017-07-27 09:36:42 -0500

    Added new Group-Policy API's

---
+++ api/controllers/group_policy.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const uuidv1 = require('uuid/v1');
+
+var kafkaConnector = require('./../dsi/kafka_connector'),
+    requestHandler = require('./../models/group_policy.js'),
+    response = require('./../helpers/response.js');
+// var errorResponseHandle = requestHandler.errorResponseHandle();
+
+//TODO - remove this when intergrated with actual Kafka
+
+/**
+ * Check swagger.yaml for the declaration of operationId policyassociation
+ * @param {*} req 
+ * @param {*} res 
+ */
+function policyassociation(req, res) {
+    console.log('policyassociation');
+    var params = req.swagger.params;
+    var orgId = params.orgid.value;
+    var siteId = params.siteid.value;
+    var associateGroupPolicyObject = req.body;
+    var messageId = uuidv1();
+
+    //Construct the payload that has to be sent to Kafka
+    var payLoad = requestHandler.postAssociateRequestHandle();
+    payLoad.messageid = messageId; // for unique messages we can overide the message id here
+    payLoad.request.orgprops.orgid = orgId;
+    payLoad.request.siteprops.siteid = siteId;
+    payLoad.request.configprops.GroupPolicyLink.groupPolicyAssociation.push(associateGroupPolicyObject);
+    console.log(associateGroupPolicyObject, '@@@@@@@@@@@@@@@@@@@');
+    console.log("payload that we send @@@@",JSON.stringify(payLoad));
+    //Send the message to Kafka. 
+    kafkaConnector.producePolicyCategoryMessage(payLoad, function (err, msg) {
+        console.log('err: ' + err + ' message: ' + msg);
+        if (!err) {
+            kafkaConnector.consumePolicyCategoryMessage(messageId, function (err, msg) {
+                response.Done(err, msg.response, res, req);
+            });
+        } else {
+            response.Done(err, msg, res, req);
+        }
+    });
+}
+
+
+/**
+ * Check swagger.yaml for the declaration of operationId policydisassociation
+ * @param {*} req 
+ * @param {*} res 
+ */
+function policydisassociation(req, res) {
+    console.log('policydisassociation');
+    var params = req.swagger.params;
+    var orgId = params.orgid.value;
+    var siteId = params.siteid.value;
+    var deassociateGroupPolicyObject = req.body;
+    var messageId = uuidv1();
+
+    //Construct the payload that has to be sent to Kafka
+    var payLoad = requestHandler.postDeassociateRequestHandle();
+    payLoad.messageid = messageId; // for unique messages we can overide the message id here
+    payLoad.request.orgprops.orgid = orgId;
+    payLoad.request.siteprops.siteid = siteId;
+    payLoad.request.configprops.GroupPolicyLink.groupPolicyAssociation.push(deassociateGroupPolicyObject);
+    //Send the message to Kafka. 
+    kafkaConnector.producePolicyCategoryMessage(payLoad, function (err, msg) {
+        console.log('err: ' + err + ' message: ' + msg);
+        if (!err) {
+            kafkaConnector.consumePolicyCategoryMessage(messageId, function (err, msg) {
+                response.Done(err, msg.response, res, req);
+            });
+        } else {
+            response.Done(err, msg, res, req);
+        }
+    });
+}
+
+
+/**
+ * Export these functions to use it from other JS
+ */
+module.exports = {
+    policyassociation: policyassociation,
+    policydisassociation: policydisassociation
+};
---
+++ api/models/group_policy.js
@@ -0,0 +1,75 @@
+'use strict';
+module.exports = {
+    postAssociateRequestHandle: postAssociateRequestHandle,
+    postDeassociateRequestHandle: postDeassociateRequestHandle
+};
+
+/**
+ * Returns the postAssociateRequestHandle for the group policy 
+ * intialise them with some dummy values
+ * These can be overidden with in each specific request
+ */
+function postAssociateRequestHandle() {
+    var postAssociateRequestHandle = {
+        "messageid": "da4067d5-7532-40d1-8763-9f19f02d4f9e",
+        "request": {
+            "instanceid": "75b3e303-ec4f-4643-b483-f98d975a5d24",
+            "responsetopic": "parking.policy.request",
+            "requestid": "291ef868-2bd0-4350-ac87-3591039bb397",
+            "timestamp": "2017-07-12T02:33:25.504Z",
+            "type": "policyassociation",
+            "model": "getSensorHistoryFromTo",
+            "action": "CAN_READ",
+            "user": "0be386cc-2cca-4a1c-9a07-0e5a9bc2306c",
+            "orgprops": {
+                "orgid": "d8547b37-95ba-410d-9382-0b190d951332"
+            },
+            "siteprops": {
+                "siteid": "d8547b37-95ba-410d-9382-0b190d951332"
+            },
+            "configprops": {
+                "GroupPolicyLink": {
+                    "groupPolicyAssociation": [
+                        
+                    ]
+                }
+            }
+        }
+    };
+    return postAssociateRequestHandle;
+}
+
+/**
+ * Returns the postDeassociateRequestHandle for the group policy 
+ * intialise them with some dummy values
+ * These can be overidden with in each specific request
+ */
+function postDeassociateRequestHandle() {
+    var postDeassociateRequestHandle = {
+        "messageid": "da4067d5-7532-40d1-8763-9f19f02d4f9e",
+        "request": {
+            "instanceid": "75b3e303-ec4f-4643-b483-f98d975a5d24",
+            "responsetopic": "parking.policy.request",
+            "requestid": "291ef868-2bd0-4350-ac87-3591039bb397",
+            "timestamp": "2017-07-12T02:33:25.504Z",
+            "type": "policydisassociation",
+            "model": "getSensorHistoryFromTo",
+            "action": "CAN_READ",
+            "user": "0be386cc-2cca-4a1c-9a07-0e5a9bc2306c",
+            "orgprops": {
+                "orgid": "d8547b37-95ba-410d-9382-0b190d951332"
+            },
+            "siteprops": {
+                "siteid": "d8547b37-95ba-410d-9382-0b190d951332"
+            },
+            "configprops": {
+                "GroupPolicyLink": {
+                    "groupPolicyAssociation": [
+                        
+                    ]
+                }
+            }
+        }
+    };
+    return postDeassociateRequestHandle;
+}
