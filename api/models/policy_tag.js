48b65e4272bcaac66b89554b4c35e7ceb23b7b1c
vasanthsunder <vasanth.sunder@verizon.com>
2017-08-02 11:26:24 -0500

    Irrelevant object removed

--- api/models/policy_tag.js
+++ api/models/policy_tag.js
@@ -31,10 +31,7 @@ function getRequestHandle() {
                 "siteid": "d8547b37-95ba-410d-9382-0b190d951332"
             },
             "configprops": {
-                "uid": "534602bf-1d76-43b0-adcb-c5d932580e05",
-                "tag": {
-                    "uid": null
-                }
+                "uid": "534602bf-1d76-43b0-adcb-c5d932580e05"
             }
         }
     };
