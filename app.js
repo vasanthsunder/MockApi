be44d5a975bb385d5e1eb5ae4a9bfdc7567b7975
vasanthsunder <vasanth.sunder@verizon.com>
2017-08-08 10:33:48 -0500

    Enabled Swagger UI

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var app = require('express')();
+var express = require('express');
 var cors = require('cors');
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
+
+app.use('/api-docs', express.static(__dirname + '/swagger/swagger-ui'));
 
 var cors_options = {
     // "origin": "http://localhost:8080",
@@ -34,6 +38,9 @@ global.log = console;
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
+
+  // Add swagger-ui (This must be before swaggerExpress.register)
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
 
   // install middleware
   swaggerExpress.register(app);
