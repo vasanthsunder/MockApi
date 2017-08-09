5a2fee31bfd0aad8464b31ff510f2741b83f634b
vasanthsunder <vasanth.sunder@verizon.com>
2017-08-09 16:20:55 -0500

    Changes needed

--- api/models/parking_spots.js
+++ api/models/parking_spots.js
@@ -85,27 +85,21 @@ function postParkingSpotRequestHandle() {
  */
 function getAllParkingSpotsRequestHandle() {
     var getAllParkingSpotsRequestHandle = {
-        "messageid": "da4067d5-7532-40d1-8763-9f19f02d4f9e",
+        "messageid": "ee647269-e302-477f-8963-8aa0317f9cd3",
         "request": {
-            "instanceid": "75b3e303-ec4f-4643-b483-f98d975a5d24",
-            "responsetopic": "parking.policy.request",
-            "requestid": "291ef868-2bd0-4350-ac87-3591039bb397",
-            "timestamp": "2017-07-12T02:33:25.504Z",
-            "type": "policydisassociation",
+            "instanceid": "ff4083b0-9648-4a4c-a089-3128a5a29a2b",
+            "responsetopic": "parking.policy.res",
+            "requestid": "711a588b-5f57-4aec-88bf-46f7fa8a997f",
+            "timestamp": "2017-08-08T20:20:45.191Z",
+            "type": "getAllParkingSpots",
             "model": "getSensorHistoryFromTo",
             "action": "CAN_READ",
-            "user": "0be386cc-2cca-4a1c-9a07-0e5a9bc2306c",
+            "user": "e25e0c39-69ab-401d-ad01-2e5257eb2164",
             "orgprops": {
                 "orgid": "d8547b37-95ba-410d-9382-0b190d951332"
             },
             "siteprops": {
                 "siteid": "d8547b37-95ba-410d-9382-0b190d951332"
-            },
-            "configprops": {
-                "policyid": "string",
-                "ParkingGroupPolicyLink": {
-                    "parkinggroupid": "string"
-                }
             }
         }
     };
